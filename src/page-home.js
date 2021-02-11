fa157e73b560513ee09f94f65b2c4d1d35db2ca2
Maximiliano <maxirpc2607@gmail.com>
2021-02-10 21:33:11 -0300

    clase 35

--- src/page-home.js
+++ src/page-home.js
@@ -3,12 +3,23 @@ import "./page-home.css";
 import logo from "./logo.svg";
 
 class PageHome extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.history.push("/busqueda?" + this.state.busqueda);
+  };
+  handleChange = (e) => {
+    this.setState({ busqueda: e.target.value });
+  };
+  state = {
+    busqueda: "",
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row centrado">
           <div className="col-md-6 centrar">
-              <img src={logo} alt="" id="logo"/>
+            <img src={logo} alt="" id="logo" />
             <form
               className="form-inline"
               onSubmit={this.handleSubmit}
@@ -21,12 +32,14 @@ class PageHome extends React.Component {
                   id="buscar"
                   value={this.props.busqueda}
                   placeholder="Busca una banda"
-                  onChange={this.props.onChange}
+                  onChange={this.handleChange}
                 />
               </div>
               <div className="actions">
-                  <button className="btng">Search Similar Artist</button>
-                  <button className="btng">EscuelaDevRock</button>
+                <button className="btng" type="submit">
+                  Search Similar Artist
+                </button>
+                <button className="btng">EscuelaDevRock</button>
               </div>
             </form>
           </div>
